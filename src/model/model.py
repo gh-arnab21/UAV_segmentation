6ce9c1885506de5bd54d935b00f75ecda660b1f9
Arnab Deka <arnabdeka4510@gmail.com>
2025-09-04 23:32:18 +0530

    Update model.py

--- src/model/model.py
+++ src/model/model.py
@@ -1,56 +1,14 @@
-"""Module providing a class to define the Fire Segmentation Model."""
-from typing import Tuple
 import torch
-from torch import nn
+import torch.nn as nn
+from torchvision.models.segmentation import deeplabv3_mobilenet_v3_large
 
-from .modules import Encoder, Decoder
-
-
-class FireSegmentationModel(nn.Module):
-    """The Fire Segmentation Model.
-
-    Attributes
-    ----------
-    encoder : Encoder
-        The encoder module
-    decoder : Decoder
-        The encoder module
-    """
-    def __init__(self, input_size: Tuple[int, int], device: str) -> None:
-        """Initialize the Fire Segmentation Model.
-
-        Parameters
-        ----------
-        input_size : (int, int)
-            The size of the input image.
-        device : str
-            The device where the model will be loaded.
-        """
+class RockfallDeepLab(nn.Module):
+    def __init__(self, num_classes=6, pretrained=True):
         super().__init__()
-        # Set the encoder module.
-        self.encoder = Encoder()
-        # Set the decoder module.
-        self.decoder = Decoder(target_size=input_size)
-        # Assign the model to the desired device.
-        self.to(device)
+        self.model = deeplabv3_mobilenet_v3_large(pretrained=pretrained)
+        # Replace classifier head
+        in_channels = self.model.classifier[4].in_channels
+        self.model.classifier[4] = nn.Conv2d(in_channels, num_classes, kernel_size=1)
 
-    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
-        """Forward pass of the Fire Segmentation Model.
-
-        Parameters
-        ----------
-        x : FloatTensor
-            The input tensor.
-
-        Returns
-        -------
-        FloatTensor
-            The output tensor containing the final segmentation mask of
-            the background and the foreground.
-        """
-        # Apply the encoder block to the input and get the
-        # intermediate features.
-        f1, f2, f3, f4 = self.encoder(x)
-        # Decode the final segmentation mask.
-        out = self.decoder(f1, f2, f3, f4)
-        return out
+    def forward(self, x):
+        return self.model(x)
